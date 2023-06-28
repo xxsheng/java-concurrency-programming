b939926c162b87ff43b75db0429b617dbf5ada62
xiaxiuqiang <1558281773@qq.com>
2023-06-28 09:27:59 +0800

    阻塞队列源码解析

--- src/main/java/chapter6/blockqueue/ArrayBlockingQueueTest.java
+++ src/main/java/chapter6/blockqueue/ArrayBlockingQueueTest.java
@@ -1,12 +1,11 @@
 package chapter6.blockqueue;
 
-import java.util.AbstractQueue;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Consumer;
 
 public class ArrayBlockingQueueTest<E> extends AbstractQueue<E>
         implements BlockingQueue<E>, java.io.Serializable  {
@@ -28,7 +27,7 @@ public class ArrayBlockingQueueTest<E> extends AbstractQueue<E>
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Itr();
     }
 
     @Override
@@ -38,34 +37,89 @@ public class ArrayBlockingQueueTest<E> extends AbstractQueue<E>
 
     @Override
     public void put(E e) throws InterruptedException {
-        lock.lock();
-        if (count == items.length) {
-            notFull.await();
+        Objects.requireNonNull(e);
+        lock.lockInterruptibly();
+        try {
+            if (count == items.length) {
+                notFull.await();
+            }
+            enqueue(e);
+        } finally {
+            lock.unlock();
         }
+    }
+
+    private void enqueue(E e) {
         items[addIndex++] = e;
+        if (addIndex == items.length) addIndex = 0;
         count++;
         notEmpty.signal();
-        lock.unlock();
     }
 
     @Override
     public boolean offer(E e, long timeout, TimeUnit unit) throws InterruptedException {
-        return false;
+        Objects.requireNonNull(e);
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            long nanos = unit.toNanos(timeout);
+            while (count == items.length) {
+                if (nanos <= 0) {
+                    return false;
+                }
+                nanos = notFull.awaitNanos(nanos);
+            }
+            enqueue(e);
+            return true;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public E take() throws InterruptedException {
-        return null;
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            while (count == 0) {
+                notEmpty.await();
+            }
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private E dequeue() {
+        E e = (E)items[removeIndex];
+        items[removeIndex] = null;
+        if (++removeIndex == items.length) removeIndex = 0;
+        count--;
+        notFull.signal();
+        return e;
     }
 
     @Override
     public E poll(long timeout, TimeUnit unit) throws InterruptedException {
-        return null;
+        final ReentrantLock lock = this.lock;
+        lock.lockInterruptibly();
+        try {
+            long nanos = unit.toNanos(timeout);
+            while (count == 0) {
+                if (nanos <= 0 ){
+                    return null;
+                }
+                nanos = notEmpty.awaitNanos(nanos);
+            }
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public int remainingCapacity() {
-        return 0;
+        return items.length - count;
     }
 
     @Override
@@ -80,16 +134,146 @@ public class ArrayBlockingQueueTest<E> extends AbstractQueue<E>
 
     @Override
     public boolean offer(E e) {
-        return false;
+        Objects.requireNonNull(e);
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            if (count == items.length) {
+                return false;
+            }
+            enqueue(e);
+            return true;
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public E poll() {
-        return null;
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            if (count == 0) {
+                return null;
+            }
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
     public E peek() {
-        return null;
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            return itemAt(removeIndex);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private E itemAt(int removeIndex) {
+        return (E)items[removeIndex];
+    }
+
+    private class Itr implements Iterator<E> {
+        int remaining;
+        int nextIndex;
+        E nextItem;
+        int lastIndex;
+        E lastItem;
+
+        public Itr() {
+            final ReentrantLock lock = ArrayBlockingQueueTest.this.lock;
+            lock.lock();
+            try {
+
+                if ((this.remaining = count) > 0) {
+
+                    this.nextIndex = removeIndex;
+                    this.nextItem = itemAt(nextIndex);
+                }
+                this.lastIndex = -1;
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return remaining>0;
+        }
+
+        @Override
+        public E next() {
+            final ReentrantLock lock = ArrayBlockingQueueTest.this.lock;
+            lock.lock();
+            try {
+                if (remaining <= 0) {
+                    throw new NoSuchElementException();
+                }
+                lastIndex = nextIndex;
+                E e = itemAt(nextIndex);
+                if (e == null) {
+                    nextItem = e;
+                    lastItem = null;
+                } else {
+                    lastItem = e;
+                }
+                while ((--remaining) > 0 && (nextItem = itemAt(nextIndex = (++nextIndex == items.length ? 0 : nextIndex))) == null)
+                    ;
+                return e;
+            } finally {
+                lock.unlock();
+            }
+        }
+
+        @Override
+        public void remove() {
+            final ReentrantLock lock = ArrayBlockingQueueTest.this.lock;
+            lock.lock();
+            try {
+                int i = lastIndex;
+                if (i == -1) {
+                    throw new IllegalStateException();
+                }
+                lastIndex = -1;
+                E x = lastItem;
+                lastItem = null;
+                if (x != null && x == itemAt(i)) {
+                    removeAt(i);
+                }
+                items[i] = null;
+
+            } finally {
+                lock.unlock();
+            }
+
+        }
+
+        private void removeAt(int i) {
+            final Object[] items = ArrayBlockingQueueTest.this.items;
+            for (;;) {
+                int nexti = inc(i);
+                if (nexti != addIndex) {
+                    items[i] = itemAt(nexti);
+                    i = nexti;
+                } else {
+                    items[i] = null;
+                    addIndex = i;
+                    break;
+                }
+            }
+        }
+
+        private int inc(int i) {
+            return ++i == items.length ? 0 : i;
+        }
+
+        @Override
+        public void forEachRemaining(Consumer<? super E> action) {
+            Iterator.super.forEachRemaining(action);
+        }
     }
 }
